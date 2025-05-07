4893604ef222d737eaaf1699a419029ae7ffb50d
whuyu <2104676791@qq.com>
2025-05-08 00:01:19 +0800

    初始

---
+++ Program.cs
@@ -0,0 +1,12 @@
+namespace SudokuGame
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new MainForm());
+        }
+    }
+} 
